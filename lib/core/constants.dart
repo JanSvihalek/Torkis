d9a35be9497bb02957480dae98074a0d98fee4c1
Jan Švihálek <jan.svihalek00@gmail.com>
2026-05-04 21:13:42 +0200

    Verze 4.0.4
    Upraveno pořadí kroků v příjmu

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String kAppVerze = '4.0.3';
+const String kAppVerze = '4.0.4';
 const String kKontaktEmail = 'podpora@torkis.cz';
 const String kKontaktTelefon = '+420 731 901 003';
 const String kKontaktWeb = 'app.torkis.cz';
